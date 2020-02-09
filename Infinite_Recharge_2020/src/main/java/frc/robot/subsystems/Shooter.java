9bd867c41ae4388234ef13876640efe1fd562a4d
ctoth <ctoth@mail.bradley.edu>
2020-02-09 16:58:42 -0600

    added ctre library

--- Infinite_Recharge_2020/src/main/java/frc/robot/subsystems/Shooter.java
+++ Infinite_Recharge_2020/src/main/java/frc/robot/subsystems/Shooter.java
@@ -8,16 +8,18 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 /**
  * Add your docs here.
  */
-public class Shooter extends Subsystem {
-  // Put methods for controlling this subsystem
-  // here. Call these from Commands.
+public class Shooter extends Subsystem 
+{
+  
 
   @Override
-  public void initDefaultCommand() {
+  public void initDefaultCommand() 
+  {
     // Set the default command for a subsystem here.
     // setDefaultCommand(new MySpecialCommand());
   }
